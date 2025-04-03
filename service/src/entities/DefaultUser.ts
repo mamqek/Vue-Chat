2e4327788b3f37711faee4ff580366b42bba5f10
mamqek <65137206+mamqek@users.noreply.github.com>
2025-04-04 00:07:04 +0200

    id is inherited from BaseUser

--- service/src/entities/DefaultUser.ts
+++ service/src/entities/DefaultUser.ts
@@ -1,16 +1,12 @@
 // src/entities/User.ts
 import {
   Entity,
-  PrimaryGeneratedColumn,
   Column,
 } from 'typeorm';
 import { BaseUser } from './BaseUser';
 
 @Entity({ name: 'users' })
 export class DefaultUser extends BaseUser {
-    @PrimaryGeneratedColumn()
-    id!: number;
-
     @Column({ type: 'text' })
     full_name!: string;
 
